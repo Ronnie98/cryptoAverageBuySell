5bff360c9c485dfcacd47751c1fe987277f9f889
Ronaldo Konmaz <ronaldokonmaz@Gmail.com>
2020-12-24 11:44:25 +0200

    Update stock_average.py
    Added some notation

--- stock_average.py
+++ stock_average.py
@@ -1,38 +1,39 @@
 
 #initialised lists used for buy
 BoughtQuantity = []
+#list to be used to track quantity bought
 BuyPrices = []
+#list to be used to track prices for bought quantity
 BoughtMultiple = []
-#BuyFees = []
+#list to be used to store the total values (price*quantity) of buys
 
 #initialised lists used for sell
 SoldQuantity = []
+#list to be used to track quantity sold
 SellPrices = []
+#list to be used to track prices for sold quantity
 SoldMultiple = []
-#SellFees = []
+#list to be used to store the total values (price*quantity) of sales
 
 
 
 #get input regarding coin name, save it in variable
 CoinName = input("Name of coin: ")
-#Contiunally go until quued to stop
 
-	
-	
-	
-
+#start while loop, set continually to True
 while True:
-
+#understand whether user wants to input buy or sell call	
 	BuySell = input("Buy or Sell?, input b or s: ")
-	
+	#if user types 'done', break and move on
 	if BuySell == 'done':
 		break
+		#error handling
 	elif BuySell != 'b' and BuySell !='s':
 		print("not valid")
 		continue
 	
 	
-	#try / except to see make sure value is numeric
+	#try / except to see make sure value is positive numeric value
 	StrQuantity = input("Num of coins: ")
 	try:
 		#save value of num of coins
@@ -42,9 +43,11 @@ while True:
 		continue
 	if Quantity < 0:
 		print ("Must be a positive amount.") 
+		#second way to break while loop
 	elif Quantity == 0 or bool(StrQuantity) == False:
 		break
-	
+
+	#error handling, to make sure price is a positive numeric value
 	try:	
 		Price = float(input("Price of coins: "))
 	except ValueError:
@@ -55,11 +58,12 @@ while True:
 		print("Must be a positive amount.") 
 		continue
 
-	
+
+	#steps to take if user is inputing a buy order.
 	if BuySell == 'b':
 		#append to the list new number of coins purchased
 		BoughtQuantity.append(Quantity)
-		#Fees.append(fee)
+		
 		
 		BuyPrices.append(Price)
 		#init price x quant to buy Bmultiple variable
@@ -67,11 +71,12 @@ while True:
 		#add this to buy - multiple list
 		BoughtMultiple.append(multiple)
 
-		#TotalBuyFees = sum(Fees)
+		
 		TotalBoughtCoins = sum(BoughtQuantity)
 		TotalBuyCost = sum(BoughtMultiple)
 		BuyAverage = TotalBuyCost / TotalBoughtCoins
 
+	#steps to take is user is inputting a sell order.
 	else:
 		pass
 		SoldQuantity.append(Quantity)
